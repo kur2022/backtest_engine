b1b28ee23e6daa4948776153d614ade586e3a8cf
kur2022 <112455143+kur2022@users.noreply.github.com>
2025-09-26 18:05:14 +0530

    Update strategy_generator.py

--- strategy_generator.py
+++ strategy_generator.py
@@ -1,18 +1,16 @@
-def generate_rule_based_strategies(buy_levels, sell_levels, open_col='Open_^NSEBANK', close_col='Close_^NSEBANK'):
+def generate_rule_based_strategies(buy_levels, sell_levels):
     strategies = []
-
     for b in buy_levels:
         for s in sell_levels:
-            def strategy(row, b=b, s=s):
-                if row[close_col] > row[open_col] * b:
+            def strategy_logic(row, buy_thresh=b, sell_thresh=s):
+                if row['Close_^NSEBANK'] > row['Open_^NSEBANK'] * buy_thresh:
                     return 'Buy'
-                elif row[close_col] < row[open_col] * s:
+                elif row['Close_^NSEBANK'] < row['Open_^NSEBANK'] * sell_thresh:
                     return 'Sell'
                 else:
                     return 'Hold'
             strategies.append({
                 "name": f"Buy>{b}, Sell<{s}",
-                "logic": strategy
+                "logic": strategy_logic
             })
-
     return strategies
