67a81feb3ac54597e890ac4b1021ff218ddcc072
KumarR <112455143+kur2022@users.noreply.github.com>
2025-10-25 10:22:08 +0530

    Fix formatting and remove unused import in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
+// pages/_app.js
 import '../styles/globals.css'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
 import { supabase } from '../utils/supabaseClient'
 
 export default function App({ Component, pageProps }) {
